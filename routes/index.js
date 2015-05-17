3dbc873eb92d33772bf865296eabc1074db11abe
Shrey Gupta <gupta.lego@gmail.com>
2015-05-16 20:17:32 -0400

    Update files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'I am a beast' });
+  res.render('index', { title: 'Tenjin' });
 });
 
 module.exports = router;
