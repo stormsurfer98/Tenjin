cbba0a0a5ed382aba5c43f7da52ff79c69f10dd3
Karthik Maiya <karthik.maiya@gmail.com>
2015-05-17 10:51:29 -0400

    Card Creator

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ server.sockets.on('connection', function(socket) {
 
     socket.on("next", function(data) {
         //console.log(data);
-        console.log((data["Recognition"]));
-        String testString = "George Washington was the first president. He helped America win the American Revolution. He also gave his Farewell Address."
+        console.log((data));
+        var testString = "George Washington was the first president. He helped America win the American Revolution. He also gave his Farewell Address."
         // if (democlientsocket != undefined) {
         //     democlientsocket.emit("next", {});
         // }           
@@ -199,3 +199,4 @@ var http = require('http');
   : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
+
---
+++ makeCards.js
@@ -0,0 +1,71 @@
+function createPersonCard(name, description){
+  $("#cardHolder").append("<div class='col-lg-3 col-md-4 col-sm-6'>
+                <div class='card card-blue'>
+                  <div class='card-main'>
+                    <div class='card-inner'>
+                      <p class='card-heading text-blue'>"+name+"</p>
+                      <p>"+ description+"
+                      </p>
+                    </div>
+                    <div class='card-action'>
+                      <ul class='nav nav-list pull-left'>
+                        <li>
+                          <a href='javascript:void(0)'><span class='access-hide'>Add</span><span class='icon icon-add'></span></a>
+                        </li>
+                        <li>
+                          <a href='javascript:void(0)'><span class='access-hide'>Add</span><span class='icon icon-favorite-outline'></span></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>");
+}
+
+function createEventCard(name, description){
+   $("#cardHolder").append("<div class='col-lg-3 col-md-4 col-sm-6'>
+                <div class='card card-green'>
+                  <div class='card-main'>
+                    <div class='card-inner'>
+                      <p class='card-heading text-blue'>"+name+"</p>
+                      <p>"+ description+"
+                      </p>
+                    </div>
+                    <div class='card-action'>
+                      <ul class='nav nav-list pull-left'>
+                        <li>
+                          <a href='javascript:void(0)'><span class='access-hide'>Add</span><span class='icon icon-add'></span></a>
+                        </li>
+                        <li>
+                          <a href='javascript:void(0)'><span class='access-hide'>Add</span><span class='icon icon-favorite-outline'></span></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>");
+}
+
+function createTermCard(name, description){
+   $("#cardHolder").append("<div class='col-lg-3 col-md-4 col-sm-6'>
+                <div class='card card-red'>
+                  <div class='card-main'>
+                    <div class='card-inner'>
+                      <p class='card-heading text-blue'>"+name+"</p>
+                      <p>"+ description+"
+                      </p>
+                    </div>
+                    <div class='card-action'>
+                      <ul class='nav nav-list pull-left'>
+                        <li>
+                          <a href='javascript:void(0)'><span class='access-hide'>Add</span><span class='icon icon-add'></span></a>
+                        </li>
+                        <li>
+                          <a href='javascript:void(0)'><span class='access-hide'>Add</span><span class='icon icon-favorite-outline'></span></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>");
+}
